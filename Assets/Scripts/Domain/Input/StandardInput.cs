dada95970860c5b38355b67bc0b85a0246a424d4
bubi <43218935+EastTemplar@users.noreply.github.com>
2023-12-09 00:45:53 +0300

    Refactor: Project structure set

---
+++ Assets/Scripts/Domain/Input/StandardInput.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids.Domain.Inputs
+{
+    public class StandardInput : MonoBehaviour, Input
+    {
+        private void Start()
+        {
+
+        }
+    }
+}
