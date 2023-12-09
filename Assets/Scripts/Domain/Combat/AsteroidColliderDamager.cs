a567246d8d865bf4bb36797dcfb0ce93d70d1c9b
bubi <43218935+EastTemplar@users.noreply.github.com>
2023-12-09 09:46:35 +0300

    fix: asteroids not being destroyed when hit player

--- Assets/Scripts/Domain/Combat/AsteroidColliderDamager.cs
+++ Assets/Scripts/Domain/Combat/AsteroidColliderDamager.cs
@@ -7,40 +7,14 @@ namespace Asteroids.Domain.Combat
     public class AsteroidColliderDamager : MonoBehaviour
     {
         [SerializeField, Min(0)] private float _timeBeforeAbleToDamageAgain = 0.75f;
-        private float _timeBeforeAbleToDamageElapsed;
-        private bool _canDamage = true;
-
-        private IEnumerator StartCanDamageCountdown()
-        {
-            _canDamage = false;
-            _timeBeforeAbleToDamageElapsed = 0;
-
-            while (_timeBeforeAbleToDamageElapsed <= 1f)
-            {
-                _timeBeforeAbleToDamageElapsed += Time.deltaTime / _timeBeforeAbleToDamageAgain;
-                yield return null;
-            }
-
-            _canDamage = true;
-        }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (_canDamage == false)
-            {
-                return;
-            }
-
             if (other.collider.attachedRigidbody.gameObject.TryGetComponent(out Health health))
             {
                 health.Damage();
-                StartCoroutine(StartCanDamageCountdown());
+                gameObject.SetActive(false);
             }
-        }
-
-        private void OnDestroy()
-        {
-            StopAllCoroutines();
         }
     }
 }
